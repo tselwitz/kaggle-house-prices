30294e503c927f09be4b8e4720b688ffc2654e8b
Todd Selwitz <selwitz.todd@gmail.com>
2023-10-06 13:24:20 -0600

    Reformats XGBoost accuracy metrics

--- XGBoost/main.py
+++ XGBoost/main.py
@@ -17,5 +17,17 @@ X_test, y_test = X[train_split:], y[train_split:]
 model = xgboost.XGBRegressor(enable_categorical=True, verbosity=0).fit(X_train, y_train)
 preds_train = model.predict(X_train)
 preds_test = model.predict(X_test)
-print("\nTrain avg. error: ",abs(sum(preds_train - y_train) / len(y_train)))
-print("Test avg. error: ", abs(sum(preds_test - y_test) / len(y_test)))
+
+def show_results(pred, y):
+  avg_real = sum(y) / len(y)
+  avg_pred = sum(pred) / len(pred)
+  avg_deviation = sum(abs(pred.T - y).T) / len(y)
+  print(f"Avg. Price (Real): ${avg_real:.2f}")
+  print(f"Avg. Price (Predicted): ${avg_pred:.2f}")
+  print(f"Avg. Deviation: ${avg_deviation:.2f}")
+  print(f"% Accuracy (Avg. Deviation / Avg. Real): {1 - (avg_deviation / avg_real):.2%}")
+
+print("\nTrain:")
+show_results(preds_train, y_train)
+print("\nTest:")
+show_results(preds_test, y_test)
